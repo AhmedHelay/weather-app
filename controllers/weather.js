28e405a4b593564fd72763fb97fd9c0aaab1df03
Ahmad Ehab <ahmad.e.helaly@gmail.com>
2022-02-07 01:49:19 +0300

    GEO api added

--- controllers/weather.js
+++ controllers/weather.js
@@ -2,8 +2,13 @@ const express = require("express");
 const router = express.Router();
 const weather = require("./../utils/weather");
 
-router.get("/", async (req, res) => {
-  response = await weather.get(req.query.city);
+router.get("/map", async (req, res) => {
+  response = await weather.getMap(req.query.city);
+  res.send(response.data);
+});
+
+router.get("/geo", async (req, res) => {
+  response = await weather.getGeo(req.query.city);
   res.send(response.data);
 });
 
--- utils/weather.js
+++ utils/weather.js
@@ -1,12 +1,16 @@
+var url;
 const axios = require("axios");
 const ACCESS_KEY = "6af490a05c95dbb5a24bd5ef32ddaa80";
+const ACCESS_TOKEN =
+  "pk.eyJ1IjoiZXNjYWxvcGEiLCJhIjoiY2t6OXE4bGNlMDRnajJvczZza2ZndzYwNSJ9.AygfK_UK3FCvFULBktptGA";
 
-async function get(city) {
+async function get(city, url, access_key_token) {
   var response;
   try {
-    response = await axios.get("http://api.weatherstack.com/current", {
+    response = await axios.get(url, {
       params: {
-        access_key: ACCESS_KEY,
+        access_key: access_key_token,
+        access_token: access_key_token,
         query: city,
       },
     });
@@ -17,4 +21,16 @@ async function get(city) {
   }
 }
 
-module.exports = { get };
+//Map
+function getMap(city) {
+  url = "http://api.weatherstack.com/current";
+  return get(city, url, ACCESS_KEY);
+}
+
+//Geo
+function getGeo(city) {
+  url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${city}.json`;
+  return get(city, url, ACCESS_TOKEN);
+}
+
+module.exports = { getMap, getGeo };
